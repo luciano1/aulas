c6bcf7a0f44b2e700b22f79e6bc59f9c9681365f
Luciano Rocha <lucianoantunesrocha@gmail.com>
2025-04-09 09:29:39 -0300

    Add files via upload

---
+++ aula3/ArcaDeNoe/src/main/java/com/mycompany/arcadenoe/Animal.java
@@ -0,0 +1,24 @@
+package com.mycompany.arcadenoe;
+
+public class Animal {
+    private String nome;
+    private String especie;
+
+    //metodo construtor
+    public Animal(String n, String e){
+        nome = n;
+        especie = e;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getEspecie(){
+        return especie;
+    }
+    public void setNome(){
+        
+    }
+    public void setEspecie(){
+        
+    }
+}
