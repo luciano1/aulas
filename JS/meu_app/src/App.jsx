8af33787b1da89122411a9b3dadeaba70a4e3a5c
Luciano Rocha <lucianoantunesrocha@gmail.com>
2026-05-05 16:55:58 -0300

    Update App.jsx

--- JS/meu_app/src/App.jsx
+++ JS/meu_app/src/App.jsx
@@ -1,11 +1,33 @@
-import Header from "./components/header";
-function App() {
+import Header from "./components/Header";
+import Aluno from "./components/Aluno";
 
+function App() {
   return (
-    <Header 
-      disciplina="Banco de Dados" 
-    />
-  )
+    <div>
+      <Header disciplina= "BD" />
+
+      <Aluno 
+        nome="Ana Clara" 
+        idade={18} 
+        curso="Desenvolvimento de Sistemas" 
+        cidade="Montes Claros" 
+      />
+
+      <Aluno 
+        nome="Carlos Eduardo" 
+        idade={20} 
+        curso="React JS" 
+        cidade="Janaúba" 
+      />
+
+      <Aluno 
+        nome="Maria Fernanda" 
+        idade={19} 
+        curso="Programação Front-End" 
+        cidade="Pirapora" 
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
